121628e92eb14e3d4abfc8ebebf574bb036f3fe4
abilionb <abilionbarros@gmail.com>
2022-05-30 21:56:34 -0300

    Update arquivos para entrega

--- restapi/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .routers import iesrouter, cursorouter
-
-urlpatterns=[
-    path('', include(iesrouter.urls)),
-    path('', include(cursorouter.urls))
-]
